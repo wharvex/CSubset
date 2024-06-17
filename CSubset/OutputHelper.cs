90429d3aef00be80774b157bdc14b9bf112c9d78
wharvex <t.gudlewski@gmail.com>
2024-06-17 19:50:14 -0400

    first commit

---
+++ CSubset/OutputHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace CSubset;
+
+public class OutputHelper
+{
+    public static string DocPath { get; } =
+        Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+    public static void DebugPrintTxt(string output, string suffix, bool append = false)
+    {
+        using var outputFile = new StreamWriter(
+            Path.Combine(DocPath, "CSubsetDebugOutput_" + suffix + ".txt"),
+            append
+        );
+        outputFile.WriteLine(output);
+    }
+}
